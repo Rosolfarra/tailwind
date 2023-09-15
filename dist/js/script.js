d3c72ba66199386cad06730e41a4eb930f424b56
Rosolfarra <rosolfarra1@gmail.com>
2023-09-15 10:30:06 +0300

    CSS TAILWIND

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const mobileBtn = document.querySelector("#mobile-btn")
+const mobilemenu = document.querySelector("#mobile-menu")
+mobileBtn.addEventListener('click' , ()=>{
+    
+        mobilemenu.classList.toggle('hidden')
+        });
